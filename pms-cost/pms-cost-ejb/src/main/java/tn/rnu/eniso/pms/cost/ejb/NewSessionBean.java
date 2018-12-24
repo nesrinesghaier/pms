5bcad772f3493e9399d5c27f03ba1b51e436f45f
bacali95 <nasreddine.bacali95@gmail.com>
2018-12-24 13:28:53 +0100

    init

--- pms-cost/pms-cost-ejb/src/main/java/tn/rnu/eniso/pms/cost/ejb/NewSessionBean.java
+++ pms-cost/pms-cost-ejb/src/main/java/tn/rnu/eniso/pms/cost/ejb/NewSessionBean.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package tn.rnu.eniso;
+package tn.rnu.eniso.pms.cost.ejb;
 
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
