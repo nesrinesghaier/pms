294ee1c632ddcb49cf4a6cb6bee588504b194ee2
ameni.souid <ameni.souid.8@gmail.com>
2019-01-03 19:54:03 +0100

    add router outlet dashboard

--- pms-client/src/main/src/app/app-routing.module.ts
+++ pms-client/src/main/src/app/app-routing.module.ts
@@ -3,9 +3,16 @@ import {Routes, RouterModule} from '@angular/router';
 import {PublicComponent} from "./public/public.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {PmsCostDashboardComponent} from "../../projects/pms-cost/src/app/pms-cost-dashboard/pms-cost-dashboard.component";
+import {MainComponent} from "./dashboard/main/main.component";
 
 const routes: Routes = [{path: '', component: PublicComponent},
-  {path: 'dashboard', component: DashboardComponent}];
+  {path: 'dashboard', component: DashboardComponent,
+  children:[
+    { path: '', component: MainComponent},
+    { path: 'cost', component: PmsCostDashboardComponent},
+    { path: 'scrum', component: PmsCostDashboardComponent},
+    { path: 'gantt', component: PmsCostDashboardComponent}
+  ]}];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- pms-client/src/main/src/app/dashboard/dashboard.component.ts
+++ pms-client/src/main/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'pms-dashboard',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) {
+  }
 
   ngOnInit() {
   }
