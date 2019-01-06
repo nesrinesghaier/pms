f69321bd2c155aaf6e6a8e1ed3a601c5267e4517
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2019-01-06 22:51:19 +0100

    add service + cors

--- pms-client/src/main/services/user.service.ts
+++ pms-client/src/main/services/user.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class UserService {
 
   constructor(private http: HttpClient) { }
 
